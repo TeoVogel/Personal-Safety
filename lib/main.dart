617d2d7a6b1a130e708fe5d635d08a05652ba578
Teo Vogel <teovogel97@gmail.com>
2022-02-21 21:25:47 -0500

    Adding Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:personal_safety/app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:personal_safety/utils/notification_service.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await NotificationService().init(); // <----
+  await NotificationService().init();
+  // TO-DO https://github.com/FirebaseExtended/flutterfire/issues/2751
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App());
 }
