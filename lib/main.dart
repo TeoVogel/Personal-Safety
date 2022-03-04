ad1291e857829ceb2802fd1b931d6b34fb16d4d1
Teo Vogel <teovogel97@gmail.com>
2022-03-04 12:26:04 -0500

    Bug fixed, removing TO-DO

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ import 'package:personal_safety/utils/notification_service.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await NotificationService().init();
-  // TO-DO https://github.com/FirebaseExtended/flutterfire/issues/2751
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
